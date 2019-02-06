f72590c127e5fe741199e5953c4fbcfd56fc02d0
Miquel Àngel Cabrera <mcabrera@capside.com>
2019-02-06 11:28:34 +0100

    Classe Pau, hem desacoplat i hem fet més funcions

---
+++ main.py
@@ -0,0 +1,17 @@
+from . import decimalToRoman2
+
+num = 345
+
+print(decimalToRoman2.convert(345))
+print(decimalToRoman2.convert(345))
+print(decimalToRoman2.convert(345))
+print(decimalToRoman2.convert(345))
+print(decimalToRoman2.convert(345))
+print(decimalToRoman2.convert(345))
+print(decimalToRoman2.convert(345))
+
+
+while num > 0 :
+    num = convert(num)
+    
+print (roman)
